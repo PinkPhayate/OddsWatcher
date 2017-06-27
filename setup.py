1f35c9246967d4e9ca5a28fb1fb090e214039e29
Tanaka Hayate <pinqphayate@gmail.com>
2017-06-27 22:55:26 +0900

    add some file to distribute

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+ 
+
+with open('LICENSE') as f:
+        license = f.read()
+setup(
+        name             = 'odds_watcher',
+        version          = '0.1.0',
+        description      = 'this module extracts odds(rate) of hourse race held today ',
+        license          = 'MIT',
+        author           = 'phayate',
+        long_description = readme,
+        author_email     = 'pinqphayat@gmail.com',
+        url              = 'https://github.com/PinkPhayate/OddsWatcher',
+        keywords         = 'hourse race odds',
+        packages         = find_packages("odds_watcher"),
+        install_requires = [bs4, request]
+        )
