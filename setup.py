9717d1c5112fac7e15a3ebf90b070a31b0f1824c
Tanaka Hayate <pinqphayate@gmail.com>
2017-06-30 00:24:06 +0900

    new version is ready to distribute

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup, find_packages
-with open('README.md') as f:
-        readme = f.read()
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-        name             = 'oddsman',
-        version          = '0.1.1',
-        description      = 'this module extracts odds(rate) of hourse race held today ',
-        license          = 'MIT',
-        author           = 'phayate',
-        long_description = readme,
-        author_email     = 'pinqphayat@gmail.com',
-        url              = 'https://github.com/PinkPhayate/OddsWatcher',
-        keywords         = 'hourse race odds',
-        packages         = find_packages(),
-        install_requires = ['bs4>=0.0.1', 'request>=0.0.13']
-        )
+    name             = 'oddsman',
+    version          = '0.1.1',
+    description      = 'this module extracts odds(rate) of hourse race held today ',
+    license          = 'MIT',
+    author           = 'phayate',
+    long_description=long_description,
+    author_email     = 'pinqphayat@gmail.com',
+    url              = 'https://github.com/PinkPhayate/OddsWatcher',
+    keywords         = 'hourse race odds',
+    packages         = find_packages('oddsman'),
+    install_requires = ['bs4>=0.0.1', 'request>=0.0.13']
+    )
